67f838ab03f803c9d4b9f1c401b6b4c3b0b12863
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-09-04 22:04:51 +0300

    Fragments removed and Highlights section added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Fragment } from 'react';
 import './Assets/styles.css';
 import Footer from './components/header_footer/Footer';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
 import VenueNfo from './components/venueNfo';
+import Highlights from './components/highlights';
 const eventDate = 'Nov, 25, 2021, 20:30:00';
 const App = () => {
   return (
     <div className='App'>
       <Header />
-      <Fragment>
-        <Featured eventDate={eventDate} />
-      </Fragment>
-      <Fragment>
-        <VenueNfo eventDate={eventDate} />
-      </Fragment>
-      <div style={{ backgroundColor: 'red', height: 800 }}>
+      <Featured eventDate={eventDate} />
+      <VenueNfo eventDate={eventDate} />
+      <Highlights />
+      {/* <div style={{ backgroundColor: 'red', height: 800 }}>
         THIS PART WILL CONTAIN EVENT TIME AND PRICES
       </div>
       <div style={{ backgroundColor: 'blue', height: 800 }}>
         THIS PART WILL CONTAIN
       </div>
       <div style={{ backgroundColor: 'green', height: 800 }}></div>
-      <div style={{ backgroundColor: 'yellow', height: 800 }}></div>
+      <div style={{ backgroundColor: 'yellow', height: 800 }}></div> */}
       <Footer />
     </div>
   );
